24f62dd1d83fd3593f72ea18801bdf4ca0eac0d9
thewongfatguy <28602677+thewongfatguy@users.noreply.github.com>
2021-04-23 17:37:53 -0300

    feat: add PaginationSink and unit tests

--- TvMazeAppLib/Package.swift
+++ TvMazeAppLib/Package.swift
@@ -7,7 +7,7 @@ let package = Package(
   name: "TvMazeAppLib",
   platforms: [.iOS(.v13)],
   products: [
-    // Products define the executables and libraries a package produces, and make them visible to other packages.
+    .library(name: "PaginationSink", targets: ["PaginationSink"]),
     .library(name: "TvMazeAppLib", targets: ["TvMazeAppLib"]),
     .library(name: "TvMazeApiClient", targets: ["TvMazeApiClient"]),
   ],
@@ -19,8 +19,13 @@ let package = Package(
     .package(url: "https://github.com/onevcat/Kingfisher.git", from: "6.0.0"),
   ],
   targets: [
-    // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-    // Targets can depend on other targets in this package, and on products in packages this package depends on.
+    // PaginationSink
+    .target(name: "PaginationSink"),
+    .testTarget(name: "PaginationSinkTests", dependencies: ["PaginationSink", "TestSupport"]),
+
+    // TestSupport
+    .target(name: "TestSupport"),
+
     .target(
       name: "TvMazeAppLib",
       dependencies: [

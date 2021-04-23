305ee6a0ce988974a2b07dbb57849e0a3376c262
thewongfatguy <28602677+thewongfatguy@users.noreply.github.com>
2021-04-23 14:04:32 -0300

    chore: init project

---
+++ TvMazeApp/TvMazeApp/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  TvMazeApp
+//
+//  Created by Guilherme Souza on 23/04/21.
+//
+
+import UIKit
+
+@main
+final class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+        return true
+    }
+}

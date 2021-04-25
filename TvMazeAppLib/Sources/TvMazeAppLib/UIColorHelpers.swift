21f6f37bec66ac645e8772180255d960a7cd3bb6
thewongfatguy <28602677+thewongfatguy@users.noreply.github.com>
2021-04-25 09:24:19 -0300

    feat: add UIColor helpers for getting css hex string

---
+++ TvMazeAppLib/Sources/TvMazeAppLib/UIColorHelpers.swift
@@ -0,0 +1,36 @@
+import UIKit
+
+extension UIColor {
+  var hex: UInt {
+    var red: CGFloat = 0
+    var green: CGFloat = 0
+    var blue: CGFloat = 0
+    var alpha: CGFloat = 0
+
+    if !getRed(&red, green: &green, blue: &blue, alpha: &alpha) {
+      getWhite(&red, alpha: &alpha)
+      green = red
+      blue = red
+    }
+
+    red = round(red * 255)
+    green = round(green * 255)
+    blue = round(blue * 255)
+    alpha = round(alpha * 255)
+
+    return UInt(alpha) << 24 | UInt(red) << 16 | UInt(green) << 8 | UInt(blue)
+  }
+
+  var hexString: String {
+    String(format: "0x%08x", hex)
+  }
+
+  var cssString: String {
+    let hex = self.hex
+    if hex & 0xFF00_0000 == 0xFF00_0000 {
+      return String(format: "#%06x", hex & 0xFFFFFF)
+    }
+
+    return String(format: "#%08x", hex)
+  }
+}

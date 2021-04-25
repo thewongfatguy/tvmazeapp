afcb084965f470eda1168bb634a90f37be310237
thewongfatguy <28602677+thewongfatguy@users.noreply.github.com>
2021-04-25 09:25:00 -0300

    feat: add some base informations to show detail view

--- TvMazeAppLib/Sources/TvMazeAppLib/ShowDetail/ShowDetailViewController.swift
+++ TvMazeAppLib/Sources/TvMazeAppLib/ShowDetail/ShowDetailViewController.swift
@@ -1,5 +1,31 @@
 import UIKit
 
+class BaseView: UIView {
+  init() {
+    super.init(frame: .zero)
+    setupViewHierarchy()
+    setupConstraints()
+    additionalConfiguration()
+  }
+
+  @available(*, unavailable)
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+
+  func setupViewHierarchy() {
+    // no-op
+  }
+
+  func setupConstraints() {
+    // no-op
+  }
+
+  func additionalConfiguration() {
+    // no-op
+  }
+}
+
 final class ShowDetailViewController: UICollectionViewController {
 
   override func viewDidLoad() {
@@ -11,9 +37,12 @@ final class ShowDetailViewController: UICollectionViewController {
   }
 }
 
-final class ShowDetailView: UIView {
+final class ShowDetailView: BaseView {
 
-  private let scrollView = UIScrollView()
+  private let scrollView = with(UIScrollView()) {
+    $0.contentInsetAdjustmentBehavior = .always
+  }
+
   private let mainHorizontalStackView = with(UIStackView()) {
     $0.axis = .vertical
   }
@@ -21,33 +50,36 @@ final class ShowDetailView: UIView {
   private let posterImageView = PosterImageView()
   private let titleInfoView = TitleInfoView()
 
-  init() {
-    super.init(frame: .zero)
-
-    setupViewHierarchy()
-    setupConstraints()
-    additionalConfiguration()
-  }
-
-  required init?(coder: NSCoder) {
-    fatalError("init(coder:) has not been implemented")
-  }
-
-  private func setupViewHierarchy() {
+  override func setupViewHierarchy() {
     addSubview(scrollView)
     scrollView.addSubview(mainHorizontalStackView)
 
     mainHorizontalStackView.addArrangedSubview(posterImageView)
+    mainHorizontalStackView.addArrangedSubview(separator())
     mainHorizontalStackView.addArrangedSubview(titleInfoView)
+    mainHorizontalStackView.addArrangedSubview(separator())
+
+    mainHorizontalStackView.addArrangedSubview(
+      ScheduleView(
+        viewModel: ScheduleView.ViewModel(
+          schedule: ScheduleView.ViewModel.Schedule(time: "22:00", days: ["Sunday", "Wednesday"]))))
+
+    mainHorizontalStackView.addArrangedSubview(separator())
+
+    mainHorizontalStackView.addArrangedSubview(
+      SummaryView(
+        summary:
+          "<p>From creators Mara Brock Akil and Kelsey Grammer, comes a comedy about three special woman, each in a relationship with one of three hard-working football players. Melanie is trying to work her relationship with Derwin, her boyfriend, while Tasha Mack balances the relationship she has with her son, Malik and with her job. Kelly and Jason are also working hard to make their relationship last.<br><b>The Game</b> focuses on each of the relationships present in these women's lives -- those with both friends and lovers -- as they learn the rules of the game.</p>"
+      ))
   }
 
-  private func setupConstraints() {
-    scrollView.edgesToSuperview(usingSafeArea: true)
+  override func setupConstraints() {
+    scrollView.edgesToSuperview()
     mainHorizontalStackView.edgesToSuperview()
     mainHorizontalStackView.width(to: self)
   }
 
-  private func additionalConfiguration() {
+  override func additionalConfiguration() {
     backgroundColor = .systemBackground
 
     posterImageView.posterURL = URL(
@@ -58,7 +90,7 @@ final class ShowDetailView: UIView {
 
 }
 
-private final class PosterImageView: UIView {
+private final class PosterImageView: BaseView {
   private let imageView = with(UIImageView()) {
     $0.contentMode = .scaleToFill
     $0.kf.indicatorType = .activity
@@ -70,26 +102,23 @@ private final class PosterImageView: UIView {
     }
   }
 
-  init() {
-    super.init(frame: .zero)
+  override func setupViewHierarchy() {
 
-    setup()
-  }
-
-  required init?(coder: NSCoder) {
-    fatalError("init(coder:) has not been implemented")
-  }
-
-  private func setup() {
-    backgroundColor = .secondarySystemBackground
     addSubview(imageView)
-    imageView.edgesToSuperview()
 
+  }
+
+  override func setupConstraints() {
+    imageView.edgesToSuperview()
     imageView.heightToWidth(of: self, multiplier: 1.47)
+  }
+
+  override func additionalConfiguration() {
+    backgroundColor = .secondarySystemBackground
   }
 }
 
-private final class TitleInfoView: UIView {
+private final class TitleInfoView: BaseView {
   private let titleLabel = with(UILabel()) {
     $0.font = .preferredFont(forTextStyle: .title2)
     $0.numberOfLines = 0
@@ -105,27 +134,19 @@ private final class TitleInfoView: UIView {
     didSet { genresView.genres = genres }
   }
 
-  init() {
-    super.init(frame: .zero)
-
-    setup()
-  }
-
-  required init?(coder: NSCoder) {
-    fatalError("init(coder:) has not been implemented")
-  }
-
-  private func setup() {
+  override func setupViewHierarchy() {
     addSubview(titleLabel)
     addSubview(genresView)
+  }
 
+  override func setupConstraints() {
     titleLabel.edgesToSuperview(excluding: .bottom, insets: .uniform(20))
     genresView.edgesToSuperview(excluding: .top, insets: .bottom(20))
     titleLabel.bottomToTop(of: genresView, offset: -8)
   }
 }
 
-private final class GenresView: UIView {
+private final class GenresView: BaseView {
   private let scrollView = with(UIScrollView()) {
     $0.contentInset = .horizontal(20)
     $0.showsHorizontalScrollIndicator = false
@@ -149,23 +170,15 @@ private final class GenresView: UIView {
     }
   }
 
-  init() {
-    super.init(frame: .zero)
-    setup()
-  }
-
-  required init?(coder: NSCoder) {
-    fatalError("init(coder:) has not been implemented")
-  }
-
-  private func setup() {
+  override func setupViewHierarchy() {
     addSubview(scrollView)
     scrollView.addSubview(stackView)
+  }
+
+  override func setupConstraints() {
     scrollView.edgesToSuperview()
     stackView.edgesToSuperview()
     stackView.height(to: self)
-
-    //        height(40)
   }
 
   private static func makeLabel(for genre: String) -> UIView {
@@ -181,5 +194,128 @@ private final class GenresView: UIView {
     label.edgesToSuperview(insets: .uniform(4))
 
     return containerView
+  }
+}
+
+private func separator() -> UIView {
+  let view = UIView()
+  view.backgroundColor = .secondarySystemBackground
+  view.height(1)
+  return view
+}
+
+private final class ScheduleView: BaseView {
+
+  struct ViewModel {
+    struct Schedule {
+      let time: String
+      let days: [String]
+    }
+
+    init(schedule: Schedule) {
+      let allWeekDays = Calendar.current.standaloneWeekdaySymbols
+      let shortWeekDays = Calendar.current.shortStandaloneWeekdaySymbols
+
+      items = zip(allWeekDays, shortWeekDays).map { weekDay, shortWeekDay in
+        Item(
+          day: shortWeekDay,
+          time: schedule.time,
+          isHighlighted: schedule.days.contains(weekDay)
+        )
+      }
+    }
+
+    struct Item {
+      let day: String
+      let time: String
+      let isHighlighted: Bool
+    }
+
+    let items: [Item]
+  }
+
+  let viewModel: ViewModel
+
+  init(viewModel: ViewModel) {
+    self.viewModel = viewModel
+    super.init()
+  }
+
+  private let containerView = UIView()
+
+  private let scrollView = with(UIScrollView()) {
+    $0.showsHorizontalScrollIndicator = false
+    $0.contentInset = .horizontal(20)
+  }
+
+  private let stackView = with(UIStackView()) {
+    $0.axis = .horizontal
+    $0.spacing = 8
+  }
+
+  override func setupViewHierarchy() {
+    addSubview(containerView)
+    containerView.addSubview(scrollView)
+    scrollView.addSubview(stackView)
+
+    viewModel.items
+      .map(ScheduleView.makeView(for:))
+      .forEach(stackView.addArrangedSubview(_:))
+  }
+
+  override func setupConstraints() {
+    containerView.edgesToSuperview()
+    scrollView.edgesToSuperview(insets: .vertical(20))
+    stackView.edgesToSuperview()
+    stackView.heightToSuperview()
+  }
+
+  private static func makeView(for item: ViewModel.Item) -> UIView {
+    let container = UIView()
+
+    container.backgroundColor = item.isHighlighted ? .systemBlue : .secondarySystemBackground
+    container.layer.cornerRadius = 4
+    container.layer.masksToBounds = true
+
+    let dayLabel = UILabel()
+    dayLabel.text = item.day
+    dayLabel.font = .preferredFont(forTextStyle: .headline)
+
+    let timeLabel = UILabel()
+    timeLabel.text = item.time
+    timeLabel.font = .preferredFont(forTextStyle: .headline)
+
+    container.addSubview(dayLabel)
+    container.addSubview(timeLabel)
+
+    dayLabel.edgesToSuperview(excluding: .bottom, insets: .uniform(4))
+    timeLabel.edgesToSuperview(excluding: .top, insets: .uniform(4))
+
+    dayLabel.bottomToTop(of: timeLabel, offset: -4)
+
+    return container
+  }
+}
+
+private final class SummaryView: BaseView {
+
+  private lazy var label = with(UILabel()) {
+    $0.numberOfLines = 0
+    $0.attributedText = summary.htmlAttributedString()
+  }
+
+  let summary: String
+
+  init(summary: String) {
+    self.summary = summary
+    super.init()
+  }
+
+  override func setupViewHierarchy() {
+    addSubview(label)
+  }
+
+  override func setupConstraints() {
+    label.edgesToSuperview(insets: .uniform(20))
   }
 }

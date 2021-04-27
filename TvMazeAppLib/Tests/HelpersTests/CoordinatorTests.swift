72a665c64efbc704488b13a8ea86382a56df41ca
thewongfatguy <28602677+thewongfatguy@users.noreply.github.com>
2021-04-27 07:17:28 -0300

    chore: use forced optional

--- TvMazeAppLib/Tests/HelpersTests/CoordinatorTests.swift
+++ TvMazeAppLib/Tests/HelpersTests/CoordinatorTests.swift
@@ -5,11 +5,11 @@ final class CoordinatorTests: XCTestCase {
 
   func test_Coordinator_ShouldNotMemLeak() {
     let coord1 = Coordinator()
-    var coord2: Coordinator? = Coordinator()
+    var coord2: Coordinator! = Coordinator()
     weak var coord3 = coord2
 
-    coord1.coordinate(to: coord2!)
-    coord2?.didFinish()
+    coord1.coordinate(to: coord2)
+    coord2.didFinish()
     coord2 = nil
 
     XCTAssertNil(coord3)
